91f13f578c9fc20f198c51a4385451f35a2f9d26
John Ferguson Smart <john.smart@wakaleo.com>
2011-12-31 15:20:48 +1100

    More details on data-driven testing

--- build.sh
+++ build.sh
@@ -9,4 +9,6 @@ cp -r figs target
 cp -r images target
 
 # Build the Single HTML Page Version
-asciidoc -b xhtml11 -a icons -a toc2 -a numbered -a pygments -o asciidoc -o target/index.html thucydides.doc 
+asciidoc -b xhtml11 -a icons -a toc2 -a numbered -a pygments -o asciidoc -o target/index.html thucydides.doc    
+
+sleep 30
