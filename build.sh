6d4f9b0ccc3649cc8b37c0c9c5694b8fb7bb7fc4
John Ferguson Smart <john.smart@wakaleo.com>
2011-12-31 12:52:17 +1100

    Use a build script instead of Gradle

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir -p target/images
+mkdir -p target/figs
+
+rm -rf target/images
+rm -rf target/figs
+cp -r figs target
+cp -r images target
+
+# Build the Single HTML Page Version
+asciidoc -b html5 -a icons -a toc2 -a numbered -a pygments -o asciidoc -o target/index.html thucydides.doc 
