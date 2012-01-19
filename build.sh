91d4a25d5e7dd78b04677aa259bb0a37820eedaa
John Ferguson Smart <john.smart@wakaleo.com>
2012-01-19 15:52:17 +1100

    Added some details about the latest improvements in JIRA integration

--- build.sh
+++ build.sh
@@ -9,6 +9,9 @@ cp -r figs target
 cp -r images target
 
 # Build the Single HTML Page Version
-asciidoc -b xhtml11 -a icons -a toc2 -a numbered -a pygments -o asciidoc -o target/index.html thucydides.doc    
+asciidoc -b xhtml11 -a icons -a toc -a numbered -a pygments -o asciidoc -o target/thucydides.html thucydides.doc    
+        
+# Build the Multipage HTML Version
+a2x -v -k -L -fchunked --xsl-file=docbook-xsl/chunked.xsl  --asciidoc-opts "-a icons -a toc -a numbered -a pygments"  --xsltproc-opts "--stringparam chunk.section.depth 1" -dbook -D target thucydides.doc   
 
 sleep 30
